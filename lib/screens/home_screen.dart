f84a34fd27cd14b6268f2f63f740d7fcaba75614
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-03-04 18:40:47 +0600

    chnaged

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("NewsApp"),
+      ),
+    );
   }
 }
