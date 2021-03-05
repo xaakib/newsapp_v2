e586094d2bf120fb34943a014b1328c64918c270
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-03-05 12:53:18 +0600

    chnaged

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,5 @@
 import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
 import 'feeds_screen.dart';
 import 'notification_screen.dart';
 import 'pages/homepage.dart';
